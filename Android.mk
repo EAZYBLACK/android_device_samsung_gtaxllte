524bc32d0913138ac3ddf231aa9f5723f8b81418
EAZYBLACK <68810731+EAZYBLACK@users.noreply.github.com>
2024-03-17 23:16:38 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter gtaxlwifi, $(TARGET_DEVICE)),)
+ifneq ($(filter gtaxllte, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
