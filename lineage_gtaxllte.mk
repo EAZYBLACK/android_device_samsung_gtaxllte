f4dfcca93d60770d32aaec39ca210653aba920b1
EAZYBLACK <68810731+EAZYBLACK@users.noreply.github.com>
2024-06-07 10:51:29 +0300

    Cleanup

--- lineage_gtaxllte.mk
+++ lineage_gtaxllte.mk
@@ -19,9 +19,8 @@ $(call inherit-product, device/samsung/gtaxllte/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
